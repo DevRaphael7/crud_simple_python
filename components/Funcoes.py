5a7be4778ac88d134c4d6f33f1152a0b5df44c46
DevRaphael7 <raphaelramalho1989@gmail.com>
2021-09-25 17:22:47 -0300

    Fazendo o INSERT

--- components/Funcoes.py
+++ components/Funcoes.py
@@ -3,9 +3,11 @@ import random
 def opcoesDoPrograma():
     print('1) Cadastrar estudante')
     print('2) Buscar estudante')
+    print('3) Ver todos os estudantes')
     print('3) Atualizar estudante')
     print("4) Notas AV1 e AV2")
     print('4) Deletar estudante')
+    print("6) Sair")
 
 
 # def mostrarInformacoes(name : str, idade : int, ra : int, periodo : str, sala :str, sexo : str, date : str):
@@ -26,6 +28,32 @@ def mostrarDados(indice : int, dicionario : dict):
     # print(f"Nome: {dicionario['nome'][indice]}")
     # print(f"Nome: {dicionario['nome'][indice]}")
     print("=================================================")
+
+def adicionarNovoEstudante(dicionario : dict, nome : str, ra : int, idade : int, periodo : str, sala : str, sexo : str, nascimento : str):
+    lista_ra = dicionario['ra']
+    lista_nome = dicionario['nome']
+    lista_idade = dicionario['idade']
+    lista_periodo = dicionario['periodo']
+    lista_sala = dicionario['sala']
+    lista_sexo = dicionario['sexo']
+    lista_nascimento = dicionario['nascimento']
+
+    lista_nome.append(nome)
+    lista_ra.append(ra)
+    lista_idade.append(idade)
+    lista_periodo.append(periodo)
+    lista_sala.append(sala)
+    lista_sexo.append(sexo)
+    lista_nascimento.append(nascimento)
+
+    dicionario['ra'] = lista_ra
+    dicionario['nome'] = lista_nome
+    dicionario['idade'] = lista_idade
+    dicionario['nascimento'] = lista_nascimento
+    dicionario['sexo'] = lista_sexo
+    dicionario['periodo'] = lista_periodo
+    dicionario['sala'] = lista_sala
+     
 
 def sortearRA():
     ra = random.randint(11111, 99999)
--- main.py
+++ main.py
@@ -23,64 +23,81 @@ if __name__ == '__main__':
     sexo = ""
     dataDeAniversario = ""
 
-    if escolha == 1:
+    while(escolha in [1, 2, 3, 4, 5, 6]):
+        if escolha == 1:
 
-        nome = input("Digite o seu nome: ")
-        idade = int(input("Digite a sua idade: "))
+            nome = input("Digite o seu nome: ")
+            idade = int(input("Digite a sua idade: "))
 
-        print("(1)Manhã\t(2)Tarde\t(3)Noite")
-        escolha_periodo = int(input("Escolha um período: "))
+            print("(1)Manhã\t(2)Tarde\t(3)Noite")
+            escolha_periodo = int(input("Escolha um período: "))
 
-        dataDeAniversario = input("Data de aniversário no formato dd/mm/yyyy: ")
+            dataDeAniversario = input("Data de aniversário no formato dd/mm/yyyy: ")
 
-        sexo = input("Seu sexo(M) ou (F): ")
+            sexo = input("Seu sexo(M) ou (F): ")
 
-        if escolha_periodo == 1:
-            periodo = "Manhã"
-        elif periodo == 2:
-            periodo = "Tarde"
-        elif periodo == 3:
-            periodo = "Noite"
-        else:
-            print("Valor inválido!")
-        
-        ra = sortearRA()
+            if escolha_periodo == 1:
+                periodo = "Manhã"
+            elif periodo == 2:
+                periodo = "Tarde"
+            elif periodo == 3:
+                periodo = "Noite"
+            else:
+                print("Valor inválido!")
+            
+            ra = sortearRA()
 
-        print("(1)Sala 01\t(2)Sala 02\t(3)Sala 03")
-        escolha_sala = int(input("Escolha uma das salas: "))
+            print("(1)Sala 01\t(2)Sala 02\t(3)Sala 03")
+            escolha_sala = int(input("Escolha uma das salas: "))
 
-        if escolha_sala == 1:
-            sala = "Sala 01"
-        elif escolha_sala == 2:
-            sala = "Sala 02"
-        elif escolha_sala == 3:
-            sala = "Sala 03"
+            if escolha_sala == 1:
+                sala = "Sala 01"
+            elif escolha_sala == 2:
+                sala = "Sala 02"
+            elif escolha_sala == 3:
+                sala = "Sala 03"
 
-        if (len(str(ra)) == 5 and nome != "" and sala != "" and sexo in ['M', 'F']):
-            est.setName(nome)
-            est.setIdade(idade)
-            est.setRa(ra)
-            est.setPeriodo(periodo)
-            est.setSala(sala)
-            est.setSexo(sexo)
-            est.setDataDeAniversario(dataDeAniversario)
-        
-        clear()
+            if (len(str(ra)) == 5 and nome != "" and sala != "" and sexo in ['M', 'F']):
+                est.setName(nome)
+                est.setIdade(idade)
+                est.setRa(ra)
+                est.setPeriodo(periodo)
+                est.setSala(sala)
+                est.setSexo(sexo)
+                est.setDataDeAniversario(dataDeAniversario)
 
-            # mostrarInformacoes(est.getName(), est.getIdade(), est.getRa(), est.getPeriodo(), est.getSala(), est.mostrarSexo(), est.getDataDeAniversario())
-    elif escolha == 2:
+                adicionarNovoEstudante(estudantes, est.getName(), est.getRa(), est.getIdade(), est.getPeriodo(), est.getSala(), est.getSexo(), est.getDataDeAniversario())
+            
+            clear()
 
-        ra_select = input("Insira o ra para buscar: ")
+            opcoesDoPrograma()
+            escolha = int(input("Opção: "))
 
-        for i in estudantes['ra']:
-            if int(ra_select) == i:
-                ind = int(estudantes['ra'].index(i))
 
-                clear()
+                # mostrarInformacoes(est.getName(), est.getIdade(), est.getRa(), est.getPeriodo(), est.getSala(), est.mostrarSexo(), est.getDataDeAniversario())
+        elif escolha == 2:
+
+            ra_select = input("Insira o ra para buscar: ")
+
+            for i in estudantes['ra']:
+                if int(ra_select) == i:
+                    ind = int(estudantes['ra'].index(i))
+                    clear()
+                    mostrarDados(ind, estudantes)
+                    break
+            
+            opcoesDoPrograma()
+            escolha = int(input("Opção: "))
+
+        elif escolha == 3:
+
+            for ra in estudantes['ra']:
+                ind = estudantes['ra'].index(ra)
+                print(f"{ra}\t{estudantes['nome'][ind]}")
+            
+            print("\n")
+            opcoesDoPrograma()
+            escolha = int(input("Opção: "))
                 
-                mostrarDados(ind, estudantes)
-
-                break
-        
 
         
